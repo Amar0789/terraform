c9448d8a1ac53025f0de52ab5eb65867ad85e96a
Amar <amarsaiyellanki789@gmail.com>
2025-12-16 17:17:38 +0530

    This is a commit message

--- terraform/ec2/main.tf
+++ terraform/ec2/main.tf
@@ -5,7 +5,7 @@ resource "aws_instance" "aws"{
     vpc_security_group_ids = [aws_security_group.allow.id]
 
     tags = {
-        Name = "(var.instances[count.index])"
+        Name = "var.instances[count.index]"
     }
 }
 
