8804d577169d5db93437434696ebc28e2e1689b5
Amar <amarsaiyellanki789@gmail.com>
2025-12-16 17:11:48 +0530

    This is a commit message

--- terraform/ec2/main.tf
+++ terraform/ec2/main.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "aws"{
     ami = var.ami
-    instance_type = var.instance == prod? "t3.micro" : "t3.small"
+    instance_type = var.instance == "prod"? "t3.micro" : "t3.small"
     vpc_security_group_ids = [aws_security_group.allow.id]
 
     tags = {
