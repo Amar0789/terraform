0918426e694ab4a9b8be9a74e7771440b1304cfc
Amar <amarsaiyellanki789@gmail.com>
2025-12-16 16:24:04 +0530

    commit message

--- terraform/ec2/main.tf
+++ terraform/ec2/main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "one"{
     ami = "ami-09c813fb71547fc4f"
     instance_type = "t3.micro"
-    vpc_security_group_ids = ["aws_security_group.allow.id"]
+    vpc_security_group_ids = [aws_security_group.allow.id]
 
     tags = {
         Name = "server"
