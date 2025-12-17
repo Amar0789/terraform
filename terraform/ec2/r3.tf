bd208b046cae1147cdc7858962a8a5b484d2e53e
Amar <amarsaiyellanki789@gmail.com>
2025-12-17 08:09:35 +0530

    commit message

--- terraform/ec2/r3.tf
+++ terraform/ec2/r3.tf
@@ -3,6 +3,6 @@ resource "aws_route53_record" "r53"{
     type = "A"
     ttl =1
     name = "${var.instances[count.index]}.${var.domain_name}"
-    records = [aws_instsnce.tf[count.index].private_ip]
+    records = [aws_instance.tf[count.index].private_ip]
     allow_overwrite = true
 }
