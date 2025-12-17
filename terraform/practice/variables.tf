00b6c63e78a95b9a4b314a11c678b716b4ed763a
Amar <amarsaiyellanki789@gmail.com>
2025-12-17 12:01:28 +0530

    commit message

---
+++ terraform/practice/variables.tf
@@ -0,0 +1,18 @@
+variable "ami"{
+    default = "ami-09c813fb71547fc4f"
+}
+
+variable "instances"{
+    default = ["mysql","backend","frontend"]
+}
+
+variable "domain_name" {
+    default = "app789.site"
+}
+variable "zone_id"{
+    default = "Z01736442JTWOVOV6NKL6"
+}
+
+variable "instance_type" {
+    default = "prod"
+}
