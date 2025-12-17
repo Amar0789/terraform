0fb4270772b2ab21ec1aeaef9b600c69577a5ba2
Amar <amarsaiyellanki789@gmail.com>
2025-12-17 15:28:14 +0530

    commit message

--- terraform/practice/main.tf
+++ terraform/practice/main.tf
@@ -1,59 +1,6 @@
-resource "aws_instance" "tf"{
-    ami = var.ami
-    instance_type = local.instance_type
-    vpc_security_group_ids = [aws_security_group.aws.id]
-
-    tags = {
-        Name = "one"
-    
-    }
-
-    connection {
-        user = "ec2-user"
-        type = "ssh"
-        host = self.public_ip
-        private_key = file("/home/ec2-user/keys")
-    }
-
-    provisioner "remote-exec"{
-        inline = [
-            "sudo dnf install tree -y",
-            "sudo dnf install nginx -y",
-            "sudo systemctl start nginx",
-        ]
-    }
-
-    provisioner "remote-exec"{
-        when = destroy
-        inline = [
-            "sudo dnf remove tree -y",
-            "sudo dnf remove nginx -y",
-        ]
-    }
-
-
-
-}
-
-resource "aws_security_group" "aws"{
-
-    egress {
-        from_port = 0
-        to_port = 0
-        protocol ="-1"
-        cidr_blocks = ["0.0.0.0/0"]
-        ipv6_cidr_blocks = ["::/0"]
-    }
-    ingress {
-        from_port = 22
-        to_port = 22
-        protocol ="tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-        ipv6_cidr_blocks = ["::/0"]
-    }
-
-    tags = {
-        Name = "something"
-    }
+module "ec2" {
+    source = "../ec2"
+    ami = var.ami_id
+    instance_type = var.instance_type
 
 }
