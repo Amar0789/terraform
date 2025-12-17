56839874fb786bfed230624fe6e57b03a8f6bd21
Amar <amarsaiyellanki789@gmail.com>
2025-12-17 08:10:46 +0530

    commit message

--- terraform/ec2/r3.tf
+++ terraform/ec2/r3.tf
@@ -1,5 +1,6 @@
 resource "aws_route53_record" "r53"{
     count = length(var.instances)
+    zone_id = "Z09912121MS725XSKH1TG"
     type = "A"
     ttl =1
     name = "${var.instances[count.index]}.${var.domain_name}"
