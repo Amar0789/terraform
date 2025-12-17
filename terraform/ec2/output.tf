78db243e0ecc8f4a41cb81c84a483bc4b92ad2ee
Amar <amarsaiyellanki789@gmail.com>
2025-12-17 15:31:14 +0530

    commit message

--- terraform/ec2/output.tf
+++ terraform/ec2/output.tf
@@ -2,7 +2,7 @@ output "ip"{
     value = aws_instance.tf.private_ip
 }
 output private_ip {
-    value = aws_instance.tf.public ip
+    value = aws_instance.tf.public_ip
 }
 output instance_id {
     value = aws_instance.tf.instance_id
