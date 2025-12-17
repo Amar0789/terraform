fa6cb6d3b30791d9b356e4ef17361cddf6af134b
Amar <amarsaiyellanki789@gmail.com>
2025-12-17 15:56:34 +0530

    commit message

--- terraform/ec2/variables.tf
+++ terraform/ec2/variables.tf
@@ -4,4 +4,8 @@ variable "ami_id"{
 
 variable "instance_type"{
     default = "t3.micro"
+    validation {
+        condition = (["t3.small" , "t3.micro"], var.instance_type)
+        error_message = "Please select only t3.small or t3.micro"
+    }
 }
