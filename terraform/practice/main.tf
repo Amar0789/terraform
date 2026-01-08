57ad4e59433eb8e28622ca240efb9c4287346ede
Amar <amarsaiyellanki789@gmail.com>
2026-01-08 19:21:22 +0530

    dd

--- terraform/practice/main.tf
+++ terraform/practice/main.tf
@@ -1,15 +1,6 @@
-resource "aws_vpc" "a"{
-    cidr_block = "10.0.0.0/16"
-    enable_dns_hostnames = true
-}
+resource "aws_lb" "main"{
+    name = "app_alb"
+    load_balancer_type = "application_load_balancer"
+    internal = false
 
-resource "aws_vpc" "b"{
-    cidr_block = "10.1.0.0/16"
-    enable_dns_hostnames = true
-}
-
-resource "aws_vpc_peering_connection" "pair"{
-    vpc_id = aws_vpc.a.id
-    peer_vpc_id = aws_vpc.b.id
-    auto_accept = true
 }
