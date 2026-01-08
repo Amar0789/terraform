fc01e4b34477af6056aa6e66420e1087340ac59f
Amar <amarsaiyellanki789@gmail.com>
2026-01-08 19:18:37 +0530

    dd

--- terraform/practice/main.tf
+++ terraform/practice/main.tf
@@ -1,30 +1,15 @@
-resource "aws_vpc" "main"{
+resource "aws_vpc" "a"{
     cidr_block = "10.0.0.0/16"
     enable_dns_hostnames = true
 }
 
-resource "aws_internet_gateway" "main"{
-    vpc_id = aws_vpc.main.id
-
+resource "aws_vpc" "b"{
+    cidr_block = "10.1.0.0/16"
+    enable_dns_hostnames = true
 }
 
-resource "aws_subnet" "public" {
-    vpc_id = aws_vpc.main.id
-    cidr_block = "10.0.1.0/24"
-    map_public_ip_on_launch = true
-}
-
-resource "aws_route_table" "main"{
-    vpc_id = aws_vpc.main.id
-}
-
-resource "aws_route" "main"{
-    route_table_id = aws_route_table.main.id
-    destination_cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.main.id
-}
-
-resource "aws_route_table_association" "main"{
-    subnet_id = aws_subnet.public.id
-    route_table_id = aws_route_table.main.id
+resource "aws_vpc_peering_connection" "pair"{
+    vpc_id = aws_vpc.a.id
+    peer_vpc_id = aws_vpc.b.id
+    auto_accept = true
 }
