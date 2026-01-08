f3f15649038836946e2f73a25d73ed2253c2e03e
Amar <amarsaiyellanki789@gmail.com>
2026-01-08 19:14:35 +0530

    dd

--- terraform/practice/main.tf
+++ terraform/practice/main.tf
@@ -20,11 +20,11 @@ resource "aws_route_table" "main"{
 
 resource "aws_route" "main"{
     route_table_id = aws_route_table.main.id
-    destination_cidr = "0.0.0.0/0"
-    gate_way_id = aws_internet_gateway.main.id
+    destination_cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
 }
 
 resource "aws_route_table_association" "main"{
-    subnet_id = aws_subnet.main.id
+    subnet_id = aws_subnet.public.id
     route_table_id = aws_route_table.main.id
 }
