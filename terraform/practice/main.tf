9e4c0821223a3401d0ab6cc254f935c3cb02bf2e
Amar <amarsaiyellanki789@gmail.com>
2026-01-08 19:35:10 +0530

    dd

--- terraform/practice/main.tf
+++ terraform/practice/main.tf
@@ -3,11 +3,11 @@ resource "aws_vpc" "main"{
     enable_dns_hostnames = true
 }
 resource "aws_internet_gateway" "main"{
-    vpc_id = "aws_vpc.main.id"
+    vpc_id = aws_vpc.main.id
 }
 
 resource "aws_subnet" "public"{
-    vpc_id = "aws_vpc.main.id"
+    vpc_id = aws_vpc.main.id
     cidr_block = "10.0.1.0/24"
     map_public_ip_on_launch = true
 
